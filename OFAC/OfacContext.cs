26187d973237e4ebd4a10b303d89e66ba8c0fdfb
Daniel Zolnjan <daniel.zolnjan@gmail.com>
2013-03-13 20:23:27 +0100

    first commit

---
+++ OFAC/OfacContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+
+namespace OFAC
+{
+    public class OfacContext : DbContext
+    {
+        public OfacContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<sdnListM> sdnList { get; set; }
+        public DbSet<sdnListPublshInformationM> sdnListPublshInformation { get; set; }
+        public DbSet<sdnListSdnEntryM> sdnListSdnEntry { get; set; }
+        public DbSet<sdnListSdnEntryIDM> sdnListSdnEntryID { get; set; }
+        public DbSet<sdnListSdnEntryAkaM> sdnListSdnEntryAka { get; set; }
+        public DbSet<sdnListSdnEntryAddressM> sdnListSdnEntryAddress { get; set; }
+        public DbSet<sdnListSdnEntryNationalityM> sdnListSdnEntryNationality { get; set; }
+        public DbSet<sdnListSdnEntryCitizenshipM> sdnListSdnEntryCitizenship { get; set; }
+        public DbSet<sdnListSdnEntryDateOfBirthItemM> sdnListSdnEntryDateOfBirthItem { get; set; }
+        public DbSet<sdnListSdnEntryPlaceOfBirthItemM> sdnListSdnEntryPlaceOfBirthItem { get; set; }
+        public DbSet<sdnListSdnEntryVesselInfoM> sdnListSdnEntryVesselInfo { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Use singular table names
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+
---
+++ OFAC/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using AutoMapper;
+
+namespace OFAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            // **** setup automapper ****
+            Mapper.CreateMap<sdnList, sdnListM>();
+            Mapper.CreateMap<sdnListPublshInformation, sdnListPublshInformationM>();
+            Mapper.CreateMap<sdnListSdnEntry, sdnListSdnEntryM>()
+                .ForMember(d => d.programList, opt => opt.MapFrom(src => String.Join(";", src.programList)));
+            Mapper.CreateMap<sdnListSdnEntryID, sdnListSdnEntryIDM>();
+            Mapper.CreateMap<sdnListSdnEntryAka, sdnListSdnEntryAkaM>();
+            Mapper.CreateMap<sdnListSdnEntryAddress, sdnListSdnEntryAddressM>();
+            Mapper.CreateMap<sdnListSdnEntryNationality, sdnListSdnEntryNationalityM>();
+            Mapper.CreateMap<sdnListSdnEntryCitizenship, sdnListSdnEntryCitizenshipM>();
+            Mapper.CreateMap<sdnListSdnEntryDateOfBirthItem, sdnListSdnEntryDateOfBirthItemM>();
+            Mapper.CreateMap<sdnListSdnEntryPlaceOfBirthItem, sdnListSdnEntryPlaceOfBirthItemM>();
+            Mapper.CreateMap<sdnListSdnEntryVesselInfo, sdnListSdnEntryVesselInfoM>();
+
+            // **** get latest data from http://www.treasury.gov/ofac/downloads/sdn.xml ****
+            Console.WriteLine("Downloading data from http://www.treasury.gov/ofac/downloads/sdn.xml");
+            var data = GetData();
+
+            // **** convert data to entity framework models ****
+            sdnListM sdnListM = Mapper.Map<sdnListM>(data);
+
+            // **** setup db and insert data ****
+            Console.WriteLine("Inserting data to db.. Please wait");
+            Database.SetInitializer<OfacContext>(new CreateDatabaseIfNotExists<OfacContext>());
+            OfacContext ofacContext = new OfacContext();
+            ofacContext.sdnList.Add(sdnListM);
+            ofacContext.SaveChanges();
+
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
+        }
+
+        public static sdnList GetData()
+        {
+            WebClient client = new WebClient();
+            string res = client.DownloadString("http://www.treasury.gov/ofac/downloads/sdn.xml");
+            File.WriteAllText(@"..\..\sdn.xml", res);
+
+            sdnList sr = new sdnList();
+
+            using (FileStream xmlStream = new FileStream(@"..\..\sdn.xml", FileMode.Open))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(xmlStream))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(sdnList));
+                    sdnList deserialized = serializer.Deserialize(xmlReader) as sdnList;
+                    return deserialized;
+                }
+            }
+        }
+    }
+}
